987e9ccf7363ec2c9c626ddda5fffd166ddb20e7
Daniel Massud <danielmassud@outlook.com>
2023-12-07 17:31:41 -0300

    FrameworkCompare agora pega deleted files

--- core/FrameworkCompare.js
+++ core/FrameworkCompare.js
@@ -3,9 +3,16 @@
  * @param {string} frameworkStr - The string output from the Framework function.
  * @returns {Map<string, string>} - A map where keys are file paths and values are file contents.
  */
+
+/**
+ * Compares two frameworks and identifies modified or new files.
+ * @param {string} baseFramework - The string output from the base Framework.
+ * @param {string} afterFramework - The string output from the after Framework.
+ * @returns {string} - A string listing modified or new files.
+ */
 function parseFrameworkString(frameworkStr) {
     const fileMap = new Map();
-    const fileSections = frameworkStr.split('//').slice(1); // Split and ignore the first empty string
+    const fileSections = frameworkStr.split('//').slice(1);
 
     for (const section of fileSections) {
         const [relativeFilePath, ...contents] = section.split('\n');
@@ -15,12 +22,6 @@ function parseFrameworkString(frameworkStr) {
     return fileMap;
 }
 
-/**
- * Compares two frameworks and identifies modified or new files.
- * @param {string} baseFramework - The string output from the base Framework.
- * @param {string} afterFramework - The string output from the after Framework.
- * @returns {string} - A string listing modified or new files.
- */
 function FrameworkCompare(baseFramework, afterFramework) {
     const baseMap = parseFrameworkString(baseFramework);
     const afterMap = parseFrameworkString(afterFramework);
@@ -30,7 +31,13 @@ function FrameworkCompare(baseFramework, afterFramework) {
         if (!baseMap.has(filePath)) {
             output += `//${filePath} - New File\n`;
         } else if (baseMap.get(filePath) !== fileContent) {
-            output += `//${filePath} - Modified\n`;
+            output += `//${filePath} - Modified\n${fileContent}\n`;
+        }
+    }
+
+    for (const filePath of baseMap.keys()) {
+        if (!afterMap.has(filePath)) {
+            output += `//${filePath} - Deleted\n`;
         }
     }
 
