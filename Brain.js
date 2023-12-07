ddbda9b60be394a052064266b95c3037e354c779
Daniel Massud <danielmassud@outlook.com>
2023-12-07 15:39:07 -0300

    Brain.Do() com prompt feito, pronto para testar como package

--- Brain.js
+++ Brain.js
@@ -22,12 +22,81 @@ class Brain {
 
     }
 
-    Do(prompt = 'Create a nodejs hello world',config = {}){
+    async Do(prompt = 'Create a nodejs hello world',config = {}){
 
-        let lasts = [{command : '',result : ''}]
+        const base_files = await this.Framework()
+
+        let lasts = [{command : '',message : '',result : ''}]
         lasts.splice(0,1)
 
-        console.log('Hello World')
+        let lasts_string = ''
+
+        let command_object = {
+            action : 'command',
+            command : '',
+            message : ''
+        }
+
+    //    while(command_object.action != 'stop'){
+
+        lasts.forEach(e => {lasts_string = `${lasts_string}
+Command : ${e.command} | Message : ${e.message} | Result : ${e.result}`})
+
+            let generate_response = await this.Main_EasyAI.Generate(`You are a AI that will work into a nodejs project (so the most of things will be in javascript, but can make any terminal command or write in another languages) and will recive a 'User :' objective and will analyze the context of files (initial files and actual after modifications and creation),system commands and last commands and predict the next command line in this format -> {"action":"command","command":"","message":""} 
+
+- Remember that you will predict only the json, nothing more than it, the text after 'AI :' tag will be only the json in the format described
+
+The next tag <<EXAMPLE>>   <</EXAMPLE>> will demonstrate some examples, but all simple, you will need consider the files,commands and last commands to predict the next to achieve the user goal
+
+// In the example below the user want a hello world, so the response is a action to write the console.log() in a file, the message field is a description of what will be made
+<<EXAMPLE>>
+User : Create a simple hello world
+AI : {"action":"command","command":"echo "console.log('hello world')" > index.js","message":"Writing a hello world in a index.js"}
+<</EXAMPLE>>
+
+// Lets go to another example, that will be a continuation of the example above, after it be writed, this generation will run again but with modification ok, so the next action will be of type 'stop', the result is empty because this command dont have response in terminal, but if a command has a response will show in result field
+<<EXAMPLE>>
+<<FILES_CHANGE>>
+index.js - New File
+console.log('hello world')
+<</FILES_CHANGE>>
+
+<<LAST_COMMANDS>>
+- Command : echo "console.log('hello world')" > index.js | Message : Writing a hello world in a index.js | Result : 
+<</LAST_COMMANDS>>
+
+User : Create a simple hello world
+AI : {"action":"stop","command":"" > index.js","message":"Hello world created successfully !"}
+<</EXAMPLE>>
+
+//Below is not more example, real aplication
+
+<<SYSTEM_INFORMATION>>
+System : ${this.Info.System}
+Commands : ${this.Info.Commands}
+<</SYSTEM_INFORMATION>>
+
+<<BASE_FILES>>
+${base_files}
+<</BASE_FILES>>
+
+<<FILES_CHANGE>>
+${this.FrameworkCompare(base_files,await this.Framework())}
+<</FILES_CHANGE>>
+
+<<LAST_COMMANDS>>
+${lasts_string}
+<</LAST_COMMANDS>>
+
+//Analyzing all the things above, now read the User objective and predict the JSON in format {"action":"command","command":"","message":""} after te 'AI :' tag, remember that you will predict only 1 JSON and stop, nothing mor
+
+User : ${prompt}
+AI : {"action"`,{stop : ['}']})
+
+     //   }
+
+console.log(generate_response)
+    
     }
 
 }
