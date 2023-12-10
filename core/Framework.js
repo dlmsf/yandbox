68286b2a0c8573a33b6e33d4bab0a3e4326a88b6
Daniel Massud <danielmassud@outlook.com>
2023-12-10 15:14:38 -0300

    config.level e config.max_tokens criadas no Framework.js e modicação no comentario do Brain.WorkType()

--- core/Framework.js
+++ core/Framework.js
@@ -20,17 +20,23 @@ class Framework {
     }
 
     // Recursive method to read files from a directory
-    static async readFiles(directory, baseFolder, config, output) {
-        const files = await fs.readdir(directory);
+    static async readFiles(directory, baseFolder, config, output, currentLevel = 0, tokenCount = 0) {
+        if (config.level !== -1 && currentLevel > config.level) {
+            return tokenCount;
+        }
 
+        const files = await fs.readdir(directory);
         if (files.length === 0) {
-            output.push(`#<># ${path.relative(baseFolder, directory)} - Empty Directory\n`);
-            return;
+            const emptyDirStr = `#<># ${path.relative(baseFolder, directory)} - Empty Directory\n`;
+            if (tokenCount + emptyDirStr.length <= config.max_tokens) {
+                output.push(emptyDirStr);
+                return tokenCount + emptyDirStr.length;
+            }
+            return tokenCount;
         }
 
         for (const file of files) {
             const relativeFilePath = path.relative(baseFolder, path.join(directory, file));
-
             if (Framework.shouldIgnore(relativeFilePath, config)) {
                 continue;
             }
@@ -39,30 +45,49 @@ class Framework {
             const fileStat = await fs.stat(filePath);
 
             if (fileStat.isDirectory()) {
-                await Framework.readFiles(filePath, baseFolder, config, output);
+                tokenCount = await Framework.readFiles(filePath, baseFolder, config, output, currentLevel + 1, tokenCount);
+                if (tokenCount >= config.max_tokens) {
+                    return tokenCount;
+                }
             } else {
                 const content = await fs.readFile(filePath, 'utf8');
-                output.push(`#<># ${relativeFilePath}\n${content}\n`);
+                const fileContentStr = `#<># ${relativeFilePath}\n${content}\n`;
+                if (tokenCount + fileContentStr.length <= config.max_tokens) {
+                    output.push(fileContentStr);
+                    tokenCount += fileContentStr.length;
+                } else {
+                    return tokenCount;
+                }
             }
         }
+        return tokenCount;
     }
 
   /**
-     * Reads the contents of files and directories from a specified folder,
-     * returning them as a string. This method is configurable to include
-     * or ignore specific files and directories.
-     *
-     * @param {Object} [config={}] - Configuration options for reading files.
-     * @param {string} [config.folderPath=Framework.getCurrentDirectory()] - The path of the folder to be read.
-     * @param {string[]} [config.ignore] - List of file paths to ignore.
-     * @param {string[]} [config.ignoreStartsWith] - List of prefix strings to ignore.
-     * @param {boolean} [config.subtitle=false] - Include a default subtitle explaining the tagging system.
-     * @param {string} [config.customSubtitle] - Custom explanatory subtitle, if provided.
-     * @returns {Promise<string>} A promise that resolves to a string containing the contents of the files.
-     */
+ * Reads the contents of files and directories from a specified folder,
+ * returning them as a string. This method is configurable to include
+ * or ignore specific files and directories.
+ *
+ * @param {Object} [config={}] - Configuration options for reading files.
+ * @param {string} [config.folderPath=Framework.getCurrentDirectory()] - The path of the folder to be read.
+ * @param {string[]} [config.ignore] - List of file paths to ignore.
+ * @param {string[]} [config.ignoreStartsWith] - List of prefix strings to ignore.
+ * @param {boolean} [config.subtitle=false] - Include a default subtitle explaining the tagging system.
+ * @param {string} [config.customSubtitle] - Custom explanatory subtitle, if provided.
+ * @param {number} [config.level=2] - The depth level for directory traversal. 
+ *                                    -1 for unlimited depth, 0 for the current directory only, 
+ *                                    1 for one level of subdirectories, etc.
+ * @param {number} [config.max_tokens=Infinity] - The maximum number of tokens allowed in the output string.
+ *                                                The token count is approximated based on characters, 
+ *                                                assuming roughly 4 characters per token.
+ * @returns {Promise<string>} A promise that resolves to a string containing the contents of the files.
+ */
+
   static async Get(config = {}) {
     const folderPath = config.folderPath || Framework.getCurrentDirectory();
     let output = [];
+    config.level = (typeof config.level === 'undefined') ? 2 : config.level;
+    config.max_tokens = (typeof config.max_tokens === 'undefined') ? Infinity : config.max_tokens;
 
     if (config.subtitle) {
         output.push(`#<># Subtitle: Each entry starts with '#<>#' followed by the file or folder name.\n`);
