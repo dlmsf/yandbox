dbee88ed3c263ba2e4005f0708f64e38a03187de
Daniel Massud <danielmassud@outlook.com>
2024-04-03 17:39:46 -0300

    Brain subindo webserver com chat funcional 100%

--- Brain.js
+++ Brain.js
@@ -1,7 +1,53 @@
-class Brain {
-    constructor(){
+import http from 'http';
+import { readFileSync } from 'fs';
+import url from 'url';
 
+class Brain {
+    constructor(processInputFunction) {
+        this.processInputFunction = processInputFunction;
+        this.messageQueue = []; // Queue of message tokens to be sent to the client
+
+        this.httpServer = http.createServer((req, res) => this.handleRequest(req, res));
+    }
+
+    async handleRequest(request, response) {
+        const parsedUrl = url.parse(request.url, true);
+
+        if (parsedUrl.pathname === '/') {
+            const chatHtml = readFileSync('./core/chat.html').toString();
+            const blankHtml = readFileSync('./core/blank.html').toString();
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(blankHtml + chatHtml);
+        } else if (parsedUrl.pathname === '/send') {
+            let body = '';
+            request.on('data', chunk => body += chunk.toString());
+            request.on('end', async () => {
+                const { message } = JSON.parse(body);
+                // Process the input and populate the queue
+                await this.processInputFunction(message, (responseMessage) => {
+                    this.messageQueue.push(...responseMessage.split(" ")); // Splitting message into tokens
+                });
+                response.writeHead(204); // No content to send back immediately
+                response.end();
+            });
+        } else if (parsedUrl.pathname === '/poll') {
+            if (this.messageQueue.length > 0) {
+                const message = this.messageQueue.shift(); // Get the next token
+                response.writeHead(200, { 'Content-Type': 'application/json' });
+                response.end(JSON.stringify({ message }));
+            } else {
+                response.writeHead(204); // No content if the queue is empty
+                response.end();
+            }
+        } else {
+            response.writeHead(404);
+            response.end();
+        }
+    }
+
+    listen(port = 3000) {
+        this.httpServer.listen(port, () => console.log(`Server listening on port ${port}`));
     }
 }
 
-export default Brain
+export default Brain;
